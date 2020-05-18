06213bf4096f3c7af95c109c1713caf6d9edf39c
poppertech <bwynne@poppertech.com>
2020-05-18 07:26:50 -0400

    Refactored Config to be Convention-Based

---
+++ HistoricalAnalysis/Config.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace HistoricalAnalysis
+{
+    public static class Config
+    {
+        public static string StartDate = "1262217600";
+        public static string EndDate = "2000000000";
+        public static string BaseDirectoryPath = @"C:\Users\bwynn\Desktop\HistoricalAnalysis";
+        public static DirectoryInfo BaseDirectory = new DirectoryInfo(BaseDirectoryPath);
+        public static DirectoryInfo[] SubDirectories = BaseDirectory.GetDirectories();
+        public static Uri BaseQueryUri = new Uri(@"https://query1.finance.yahoo.com/v7/finance/download/");
+        public static string[] StockTickers = new[] { "SPY", "UNH" };
+        public static string ParentTicker = StockTickers[0];
+    }
+}
---
+++ HistoricalAnalysis/ReturnsFileWriter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace HistoricalAnalysis
+{
+    public static class ReturnsFilesWriter
+    {
+        public static void CreateReturnsFiles()
+        {
+            var rawDataDirectory = Config.SubDirectories[0];
+            var returnsDirectory = Config.SubDirectories[1];
+            var rawDataFiles = rawDataDirectory.GetFiles();
+
+            foreach (var rawDataFile in rawDataFiles)
+            {
+                var returnsFileContent = CreateReturnsFileContent(rawDataFile);
+                var returnsFilePath = Path.Combine(returnsDirectory.FullName, rawDataFile.Name);
+                File.WriteAllText(returnsFilePath, returnsFileContent);
+            }
+
+        }
+
+        private static string CreateReturnsFileContent(FileInfo fileInfo)
+        {
+            const int firstLine = 1;
+            var stringBuilder = new StringBuilder();
+            var lines = File.ReadAllLines(fileInfo.FullName);
+
+            decimal lastClose = 0;
+            for (int cnt = firstLine; cnt < lines.Length; cnt++)
+            {
+                var line = lines[cnt];
+                var splitLine = line.Split(",");
+                var date = ParseDate(splitLine);
+                var adjustedClose = ParseAdjustedClose(splitLine);
+                if (cnt == firstLine && date.HasValue && adjustedClose.HasValue)
+                {
+                    lastClose = adjustedClose.Value;
+                }
+                else if (cnt > firstLine && date.HasValue && adjustedClose.HasValue && lastClose != adjustedClose)
+                {
+                    var totalReturn = adjustedClose / lastClose - 1;
+                    lastClose = adjustedClose.Value;
+                    stringBuilder.AppendLine(string.Join(",", date.Value.ToString("MM/dd/yyyy", DateTimeFormatInfo.InvariantInfo), totalReturn));
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private static DateTime? ParseDate(string[] splitLine)
+        {
+            var isParsedDate = DateTime.TryParse(splitLine[0], out DateTime date);
+            if (isParsedDate && date > DateTime.MinValue)
+                return date;
+
+            return null;
+        }
+
+        private static decimal? ParseAdjustedClose(string[] splitLine)
+        {
+            var isParsedAdjustedClose = decimal.TryParse(splitLine[5], out decimal adjustedClose);
+            if (isParsedAdjustedClose && adjustedClose > 0)
+                return adjustedClose;
+
+            return null;
+        }
+    }
+}
