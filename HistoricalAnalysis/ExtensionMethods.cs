9b7e1b9c1202378e153dc244bb57b9102dbc17c4
poppertech <bwynne@poppertech.com>
2020-05-21 06:20:25 -0400

    Added extension methods for Downloading and Deleting Data

--- HistoricalAnalysis/ExtensionMethods.cs
+++ HistoricalAnalysis/ExtensionMethods.cs
@@ -1,11 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Text;
 
 namespace HistoricalAnalysis
 {
     public static class ExtensionMethods
     {
+        public static void DeleteExistingData(this DirectoryInfo[] directoryInfos)
+        {
+            foreach (var directoryInfo in directoryInfos)
+                Array.ForEach(directoryInfo.GetFiles(), (fileInfo) => fileInfo.Delete());
+        }
+
+        public static void DownloadRawData(this string[] fileNames)
+        {
+            foreach (var fileName in fileNames)
+            {
+                var filePath = $@"{Config.SubDirectories[0].FullName}\{fileName}.csv";
+                var relativeUri = $"{fileName}?period1={Config.StartDate}&period2={Config.EndDate}&interval=1d&events=history";
+                var url = new Uri(Config.BaseQueryUri, relativeUri);
+                using (var webClient = new WebClient())
+                {
+                    webClient.DownloadFile(url, filePath);
+                }
+            }
+        }
+
         public static DateTime? ParseDate(this string dateString)
         {
             var isParsedDate = DateTime.TryParse(dateString, out DateTime date);
--- HistoricalAnalysis/Program.cs
+++ HistoricalAnalysis/Program.cs
@@ -7,32 +7,10 @@ namespace HistoricalAnalysis
     {
         static void Main(string[] args)
         {
-            //DeleteExistingData();
-            //DownloadRawData();
+            //Config.SubDirectories.DeleteExistingData();
+            //Config.StockTickers.DownloadRawData();
             //ReturnsFilesWriter.CreateReturnsFiles();
             var returnsDictionaries = ReturnsFileReader.CreateReturnsDictionaries();
-        }
-
-        public static void DeleteExistingData()
-        {
-            foreach (var directoryInfo in Config.SubDirectories)
-                Array.ForEach(directoryInfo.GetFiles(), (fileInfo) => fileInfo.Delete());
-
-        }
-
-        public static void DownloadRawData()
-        {
-            var directory = Config.SubDirectories[0];
-            foreach (var stockTicker in Config.StockTickers)
-            {
-                var fileName = $@"{directory.FullName}\{stockTicker}.csv";
-                var relativeUri = $"{stockTicker}?period1={Config.StartDate}&period2={Config.EndDate}&interval=1d&events=history";
-                var url = new Uri(Config.BaseQueryUri, relativeUri);
-                using (var webClient = new WebClient())
-                {
-                    webClient.DownloadFile(url, fileName);
-                }
-            }
         }
 
     }
